1b611e2efb69f55bb580a614a485a64e519e1812
Naeem <naeem.cse7.bu@gmail.com>
2025-03-09 04:44:53 -0600

    Added Editor

--- src/app/(WithDashboardLayout)/dashboard/components/Header.tsx
+++ src/app/(WithDashboardLayout)/dashboard/components/Header.tsx
@@ -31,12 +31,7 @@ const Header = ({ toggleSidebar }: { toggleSidebar: () => void }) => {
           <Menu className="h-5 w-5" />
         </Button>
         <div className="relative">
-          <Search className="absolute left-2 top-1/2 transform -translate-y-1/2 text-gray-400" />
-          <Input
-            type="search"
-            placeholder="Search..."
-            className="pl-8 pr-4 py-2 rounded-md border border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
-          />
+          
         </div>
       </div>
       <div className="flex items-center space-x-4">
