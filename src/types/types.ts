accb115a9710c3d0559aa899596e2b3cbe164857
Naeem <naeem.cse7.bu@gmail.com>
2025-03-19 04:04:58 -0600

    Added Functionality in Dashboard & Disply Public Notices

--- src/types/types.ts
+++ src/types/types.ts
@@ -3,70 +3,80 @@ export type Widget = {
   title?: string;
   content?: string;
   category?: string;
-  noticeIds?: string;
+  noticeIds?: string[] | string;
   topNotices?: TNotice[];
   notices?: TNotice[];
+  settings?: string[] | {
+    fontColor: string;
+    borderColor: string;
+    borderWidth: number;
+    cardOpacity: number;
+    noticeCount: number;
+    backgroundColor: string;
+    backgroundOpacity: number;
+  };
 };
 
 export type AspectRatio = '4:3' | '16:9' | '16:10';
 
 export type Category = {
   name: string;
-  id: string
+  id: string;
 };
 
-
 export type TNotice = {
-  id: string
-  title: string
-  content: string
-  category: string
-  categoryId: string
-  createdAt?: Date
-
-}
-
-
+  id: string;
+  title: string;
+  content: string;
+  category: string;
+  categoryId: string;
+  createdAt?: Date;
+};
 
 export type TDashboard = {
-  aspectRatio: string | null
+  aspectRatio: string | null;
   containers: {
-    i: string
-    h: number
-    w: number
-    x: number
-    y: number
-    title: string | undefined
-    id: string
-    category: string | undefined
-    noticeIds: string | undefined
-    width: string
-    height: string
-  }[]
-}
-
+    i: string;
+    h: number;
+    w: number;
+    x: number;
+    y: number;
+    title: string | undefined;
+    id: string;
+    category: string | undefined;
+    noticeIds: string | undefined;
+    width: string;
+    height: string;
+  }[];
+};
 
 export type TDashboard2 = {
-  aspectRatio: string | null,
-  notices: TNotice[],
+  aspectRatio: string | null;
+  notices: TNotice[];
   containers: {
-    i: string
-    h: number
-    w: number
-    x: number
-    y: number
-    title: string | undefined
-    id: string
-    category: string | undefined
-    noticeIds: string[]
-    width: string
-    height: string
-    leftPercent: string 
-    leftPx: string 
-    topPercent: string 
-    topPx: string 
-    
-  }[]
-}
-
-
+    i: string;
+    h: number;
+    w: number;
+    x: number;
+    y: number;
+    title: string | undefined;
+    id: string;
+    category: string | undefined;
+    noticeIds: string[];
+    width: string;
+    height: string;
+    leftPercent: string;
+    leftPx: string;
+    topPercent: string;
+    topPx: string;
+    settings?: {
+      fontColor: string;
+      borderColor: string;
+      borderWidth: number;
+      cardOpacity: number;
+      noticeCount: number;
+      backgroundColor: string;
+      backgroundOpacity: number;
+    };
+  }[];
+};
